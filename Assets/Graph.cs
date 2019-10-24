1b1af84097f00e78c758024cc2af17789d74c5a1
Richard Barlow <richard@progressinteractive.com>
2019-10-24 09:40:45 +0200

    Added graph updater that automatically creates graph edges for adjacent nodes, see gif below https://gyazo.com/1d0a778671ae763e310ddce706224e7e

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -5,7 +5,19 @@ using System.Linq;
 
 public class Graph : MonoBehaviour
 {
+    [System.Serializable]
+    public class Edge
+    {
+        public GraphNode StartNode;
+
+        public GraphNode EndNode;
+
+        [Range(0f, 10f)]
+        public int Weight;
+    }
+
     public List<GraphNode> Nodes;
+    public List<Edge> Edges; 
 
     // Start is called before the first frame update
     void Start()
