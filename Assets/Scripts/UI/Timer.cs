f10b17aafe0718c9d44ad5c6e8199801171951e4
askelej <askeltechno@hotmail.com>
2019-11-21 21:31:29 +0100

    functional timer added

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+        text.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        text.text = timer.ToString("F");
+    }
+}
