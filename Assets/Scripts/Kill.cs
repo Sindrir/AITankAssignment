2ea733966a5f18fd0a60cc083a78342fcb22416b
askelej <askeltechno@hotmail.com>
2019-11-27 01:59:43 +0100

    Kill script that detects when FieldOfView has enemy in sight

---
+++ Assets/Scripts/Kill.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public bool seesTarget;
+    [SerializeField] public GameObject enemy;
+    private  FieldOfView thisTanksView;
+    void Start()
+    {
+        seesTarget = false;
+        thisTanksView = GetComponent<FieldOfView>();
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+       
+      //  thisTanksView = GetComponent<FieldOfView>();
+       seesTarget = false;
+       for (int i = 0; i < thisTanksView.visibleTargets.Count; i++){
+           if(thisTanksView.visibleTargets[i].gameObject == enemy ) seesTarget = true;
+       }
+        
+    }
+}
